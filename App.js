7b4052389ba2b8767e1486821c350013e6381b0d
emedd33 <eskild.emedd33@gmail.com>
2020-09-05 13:45:40 +0200

    pack in drawer navigator into single component

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import WorkoutStack from './src/routes/WorkoutStack';
-import RoutineStack from './src/routes/RoutineStack';
-import ProfileStack from './src/routes/ProfileStack';
-import DrawerContent from './src/components/organisms/DrawerContent';
+
 import configureStore from './src/redux/store';
 import { Provider } from "react-redux";
-import ExerciseStack from './src/routes/ExerciseStack';
+import RootNavigator from './src/routes/RootNavigator';
 
-const Drawer = createDrawerNavigator();
 
 const store = configureStore()
 function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Exercises" drawerContent={(props) => <DrawerContent {...props} />}>
-          <Drawer.Screen name="Profile" component={ProfileStack} />
-          <Drawer.Screen name="Routines" component={RoutineStack} />
-          <Drawer.Screen name="Workouts" component={WorkoutStack} />
-          <Drawer.Screen name="Exercises" component={ExerciseStack} />
-        </Drawer.Navigator>
-      </NavigationContainer>
+      <RootNavigator />
     </Provider>
   );
 }
---
+++ src/routes/RootNavigator.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import WorkoutStack from '../routes/WorkoutStack';
+import RoutineStack from '../routes/RoutineStack';
+import ProfileStack from '../routes/ProfileStack';
+import ExerciseStack from '../routes/ExerciseStack';
+import DrawerContent from '../components/organisms/DrawerContent';
+
+const Drawer = createDrawerNavigator();
+
+const RootNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Exercises" drawerContent={(props) => <DrawerContent {...props} />}>
+                <Drawer.Screen name="Profile" component={ProfileStack} />
+                <Drawer.Screen name="Routines" component={RoutineStack} />
+                <Drawer.Screen name="Workouts" component={WorkoutStack} />
+                <Drawer.Screen name="Exercises" component={ExerciseStack} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default RootNavigator;
